6ebb2dd2e8009aaf1e71d6f8e646d09661061063
tarsandee <148780012+tarsandee@users.noreply.github.com>
2024-12-06 03:24:10 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -17,11 +17,31 @@ sudo systemctl start docker
 echo "Adding ec2-user to docker group..."
 sudo usermod -aG docker ec2-user
 
-# Pull and run the Docker container
-
 # Fetch the EC2 public IP from the metadata service
 EC2_PUBLIC_IP=$(curl -s http://ifconfig.me)
+
+# Generate the PREVIEW_SECRET (a random 32-byte hexadecimal string)
+PREVIEW_SECRET=$(openssl rand -hex 32)
 
 # Output the EC2 public IP
 echo "Setup complete. Docker container is running."
 echo "EC2 Public IP Address: $EC2_PUBLIC_IP"
+echo "Generated PREVIEW_SECRET: $PREVIEW_SECRET"
+
+# Run the Strapi Docker container
+echo "Running Strapi Docker container..."
+sudo docker run -d \
+  -e STRAPI_ADMIN_CLIENT_URL=http://$EC2_PUBLIC_IP:3000 \
+  -e STRAPI_ADMIN_CLIENT_PREVIEW_SECRET=$PREVIEW_SECRET \
+  -e HOST=0.0.0.0 \
+  -e PORT=1337 \
+  -p 1337:1337 \
+  ${{ secrets.DOCKER_USERNAME }}/cs360_foodadvisor_api:latest
+
+# Run the Next.js client Docker container
+echo "Running Next.js client Docker container..."
+sudo docker run -d \
+  -e PREVIEW_SECRET=$PREVIEW_SECRET \
+  -e NEXT_PUBLIC_API_URL=http://$EC2_PUBLIC_IP:1337 \
+  -p 3000:3000 \
+  ${{ secrets.DOCKER_USERNAME }}/cs360_foodadvisor_client:latest
