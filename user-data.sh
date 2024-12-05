c4ab0dea4622734790c66f423b9c9198ecad2e1a
tarsandee <148780012+tarsandee@users.noreply.github.com>
2024-12-06 03:58:38 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -16,34 +16,3 @@ sudo systemctl start docker
 # Add the ec2-user to the docker group to allow running Docker without sudo
 echo "Adding ec2-user to docker group..."
 sudo usermod -aG docker ec2-user
-
-# Fetch the EC2 public IP from the metadata service
-EC2_PUBLIC_IP=$(curl -s http://ifconfig.me)
-
-# Generate the PREVIEW_SECRET (a random 32-byte hexadecimal string)
-secret=$(openssl rand -base64 32)
-
-# Output the EC2 public IP
-echo "Setup complete. Docker container is running."
-echo "EC2 Public IP Address: $EC2_PUBLIC_IP"
-echo "Generated PREVIEW_SECRET: $secret"
-
-# Create an env file for API
-apiENVLocation=./api/.env
-echo HOST=0.0.0.0 > $apiENVLocation
-echo PORT=1337 >> $apiENVLocation
-echo STRAPI_ADMIN_CLIENT_URL=http://${publicIPv4}:3000 >> $apiENVLocation
-echo STRAPI_ADMIN_CLIENT_PREVIEW_SECRET=${secret} >> $apiENVLocation
-
-# Create an env file for Client
-clientENVLocation=./client/.env
-echo NEXT_PUBLIC_API_URL=http://${publicIPv4}:1337 > $clientENVLocation
-echo PREVIEW_SECRET=${secret}  >> $clientENVLocation
-
-# Run the Strapi Docker container
-echo "Running Strapi Docker container..."
-sudo docker run -d -p 1337:1337 chayanonkhanrit/cs360_foodadvisor_api:latest
-
-# Run the Next.js client Docker container
-echo "Running Next.js client Docker container..."
-sudo docker run -d -p 3000:3000 chayanonkhanrit/cs360_foodadvisor_client:latest
